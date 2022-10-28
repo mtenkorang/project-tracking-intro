bae1c36b93ca06ee6f1e4e5754c0e6be8a857f0b
Moses <moses.tenkorang@amalitech.org>
2022-10-28 19:03:54 +0000

    Update index.js

--- index.js
+++ index.js
@@ -7,16 +7,17 @@ const navBox = document.querySelector("nav");
 burgerIcon.addEventListener("click", doShowNav);
 
 function doShowNav() {
-  navBox.classList.remove("hidden");
+  //   navBox.classList.remove("hidden");
   closeIcon.classList.remove("hidden");
   burgerIcon.classList.add("hidden");
+  navBox.style.display = "block";
 }
 
 //Add click event listener to close icon
 closeIcon.addEventListener("click", doShowBurger);
 
 function doShowBurger() {
-  navBox.classList.add("hidden");
+  navBox.style.display = "none";
   closeIcon.classList.add("hidden");
   burgerIcon.classList.remove("hidden");
 }
